0088e9e319109640d8b2fcacaf4e43fdac6f2dab
Leo Young <420760135@qq.com>
2018-02-01 18:58:58 +0800

    setup typescript environment

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello');
+import * as Koa from 'koa';
+const app: Koa = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+const port = 3000;
+app.listen(port);
+console.log(`server listening on port ${port} ...`);
