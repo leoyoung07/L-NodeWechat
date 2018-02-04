a39dd13226b99a879d036da26018ad034b43b193
Leo Young <420760135@qq.com>
2018-02-04 12:03:29 +0800

    gulp use npm scripts

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,11 +2,11 @@ import { spawn } from 'child_process';
 import gulp from 'gulp';
 
 gulp.task('watch', async () => {
-    return spawn('tsc.cmd', ['--watch', '--pretty'], { stdio: 'inherit'});
+    return spawn('npm', ['run', 'watch'], { stdio: 'inherit'});
 });
 
 gulp.task('hot-reload', async () => {
-    return spawn('supervisor.cmd', ['-w', 'dist', 'dist/index.js'], { stdio: 'inherit'});
+    return spawn('npm', ['run', 'hot-reload'], { stdio: 'inherit'});
 });
 
 gulp.task('default', gulp.parallel(['watch', 'hot-reload']));
