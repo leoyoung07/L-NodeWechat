443a530bfb57fc1072aa3dbd7bc953a1d4fe4cfd
Leo Young <420760135@qq.com>
2018-02-21 00:43:58 +0800

    feat: serve static files

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,11 +2,11 @@ import { spawn } from 'child_process';
 import gulp from 'gulp';
 
 gulp.task('watch', async () => {
-    return spawn('npm', ['run', 'watch'], { stdio: 'inherit'});
+    return spawn('npm.cmd', ['run', 'watch'], { stdio: 'inherit'});
 });
 
 gulp.task('hot-reload', async () => {
-    return spawn('npm', ['run', 'hot-reload'], { stdio: 'inherit'});
+    return spawn('npm.cmd', ['run', 'hot-reload'], { stdio: 'inherit'});
 });
 
 gulp.task('default', gulp.parallel(['watch', 'hot-reload']));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Koa from 'koa';
+import KoaStatic from 'koa-static';
 const app: Koa = new Koa();
 
 app.use(async (ctx, next) => {
@@ -12,7 +13,10 @@ app.use(async (ctx, next) => {
   console.log('end: ' + new Date().getTime());
 });
 
+app.use(KoaStatic(__dirname, {format: false, index: 'index.html'}));
+
 app.use(async ctx => {
+  // TODO: redirect /admin => /admin/
   ctx.type = 'text/html';
   ctx.body = '<h1>Hello, world</h1>';
 });
