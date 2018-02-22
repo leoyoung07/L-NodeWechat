baafe26db6b8725b2dcff8b31128548f5d077554
Leo Young <420760135@qq.com>
2018-02-22 16:51:46 +0800

    feat: add wechat callback handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import wechat from 'co-wechat';
 import Koa from 'koa';
 import KoaStatic from 'koa-static';
+import config from '../config/config.json';
 const app: Koa = new Koa();
 
 app.use(async (ctx, next) => {
@@ -14,6 +16,31 @@ app.use(async (ctx, next) => {
 });
 
 app.use(KoaStatic(__dirname, {format: false, index: 'index.html'}));
+
+app.use(async (ctx, next) => {
+  if (ctx.url.indexOf('/service/wx_callback') >= 0) {
+    await wechat({
+      appid: config.appid,
+      encodingAESKey: config.encodingAESKey,
+      token: config.token
+    }).middleware(async (message, context) => {
+      let response = '';
+      if (typeof message === 'string') {
+        response = message;
+      } else if (message.MsgType === 'text') {
+        response = message.Content;
+      } else {
+        response = JSON.stringify(message);
+      }
+      return {
+        content: response,
+        type: 'text'
+      };
+    })(ctx, next);
+  } else {
+    await next();
+  }
+});
 
 app.use(async ctx => {
   // TODO: redirect /admin => /admin/
---
+++ src/typings.d.ts
@@ -0,0 +1,6 @@
+declare module 'co-wechat';
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
