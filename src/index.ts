06e6c5843d8643bcd97959bcca7199fa9bb3690d
Leo Young <420760135@qq.com>
2018-02-22 18:11:48 +0800

    refactor: add router

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
 import wechat from 'co-wechat';
 import Koa from 'koa';
+import Router from 'koa-router';
 import KoaStatic from 'koa-static';
 import config from '../config/config.json';
+
 const app: Koa = new Koa();
+const router: Router = new Router();
+
+const wechatCallback = wechat({
+  appid: config.appid,
+  encodingAESKey: config.encodingAESKey,
+  token: config.token
+}).middleware(async (message, context) => {
+  let response = '';
+  if (typeof message === 'string') {
+    response = message;
+  } else if (message.MsgType === 'text') {
+    response = message.Content;
+  } else {
+    response = JSON.stringify(message);
+  }
+  return {
+    content: response,
+    type: 'text'
+  };
+});
+
+router.all(/\/admin.*/gi, KoaStatic(__dirname, {format: false, index: 'index.html'}));
+
+router.all('/service/wx_callback', wechatCallback);
+
+router.all(/\/.*/gi, ctx => {
+  ctx.type = 'text/html';
+  ctx.body = '<h1>Hello, world</h1>';
+});
 
 app.use(async (ctx, next) => {
   console.log(ctx.url);
@@ -15,38 +46,8 @@ app.use(async (ctx, next) => {
   console.log('end: ' + new Date().getTime());
 });
 
-app.use(KoaStatic(__dirname, {format: false, index: 'index.html'}));
-
-app.use(async (ctx, next) => {
-  if (ctx.url.indexOf('/service/wx_callback') >= 0) {
-    await wechat({
-      appid: config.appid,
-      encodingAESKey: config.encodingAESKey,
-      token: config.token
-    }).middleware(async (message, context) => {
-      let response = '';
-      if (typeof message === 'string') {
-        response = message;
-      } else if (message.MsgType === 'text') {
-        response = message.Content;
-      } else {
-        response = JSON.stringify(message);
-      }
-      return {
-        content: response,
-        type: 'text'
-      };
-    })(ctx, next);
-  } else {
-    await next();
-  }
-});
-
-app.use(async ctx => {
-  // TODO: redirect /admin => /admin/
-  ctx.type = 'text/html';
-  ctx.body = '<h1>Hello, world</h1>';
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 const port = 3000;
 app.listen(port);
